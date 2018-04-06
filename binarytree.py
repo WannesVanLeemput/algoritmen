03cbfde4132cfff76579dbd7c4a34beff09686da
WannesVanLeemput <38138696+WannesVanLeemput@users.noreply.github.com>
2018-04-06 19:13:16 +0200

    Update binarytree.py

--- binarytree.py
+++ binarytree.py
@@ -17,9 +17,37 @@ class BinaryTree():
         return self._count
         
     def insert(self, key, value):
-        """complete this function"""
-        pass
+        if self.get(key):
+            self.get(key)._values.append(value)
+        self._count += 1
+        z = BinaryTreeNode(key, value)
+        y = None
+        x = self._root
+        while x != None:
+            y = x
+            if key < x._key:
+                x = x._left
+            else:
+                x = x._right
+        if y == None:
+            self._root = z
+        elif z._key < y._key:
+            y._left = z
+        elif z._key > y._key:
+            y._right = z
+    
                       
     def get(self, key):
-        """complete this function"""
-        pass
+        x = self._root
+        if x._key == key:
+            return self._root
+        while x != None:
+            if key < x._key:
+                x = x._left
+                if x._key == key:
+                    return x
+            else:
+                x = x._right
+                if x._key == key:
+                    return x
+        return None
